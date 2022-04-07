c5b2b95c6a1695a926358f11da90d6a3f42a8574
Chaedie <88099590+Chaedie@users.noreply.github.com>
2022-04-07 12:04:32 +0900

    Add files via upload

---
+++ 4_Python/syntax/string.py
@@ -0,0 +1,25 @@
+print('Hello world')
+print("Hello world")
+
+print('Hell'+'o'+' world')
+print("Hell'o' world")
+
+#escape
+print("Hell'o' \"w\"orld")
+
+#newline
+print('H')
+print('e')
+print('l')
+print('l')
+print('o')
+print()
+
+print('hel\nlo')
+#docstring
+print('''
+h
+e
+l
+l
+o''')
---
+++ 4_Python/syntax/string_process.py
@@ -0,0 +1,13 @@
+#length
+a = 'Hello Python'
+print()
+print(a)
+print()
+print(len(a))
+print()
+#index
+print(a[0])
+print(a[1])
+print(a[2:5])
+#repeat
+print((a+'\n')*2)
