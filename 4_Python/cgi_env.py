fb1dfaeb60823d2b60b162082fb57ac4779cb0ef
Chaedie <88099590+Chaedie@users.noreply.github.com>
2022-04-07 22:35:57 +0900

    Add files via upload

---
+++ 4_Python/cgi_env.py
@@ -0,0 +1,7 @@
+#!C:\Program Files\Python310\python.exe
+print("Content-Type: text/html")    # HTML is following
+print()                             # blank line, end of headers
+
+import cgi
+cgi.test()
+``
