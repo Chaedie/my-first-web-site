048fbd1ffe4b262b27ce928d0889d18604fef38c
Chaedie <88099590+Chaedie@users.noreply.github.com>
2022-04-08 11:08:35 +0900

    Finish py3 Lecture by 'opentutorial.org'

---
+++ 4_Python/syntax/math2.py
@@ -0,0 +1,14 @@
+#code
+def average(a,b,c):
+    s = a+b+c
+    r = s/3
+    return r
+
+def plus(a,b):
+    return a+b
+
+pi = 3.14
+
+#code
+
+print(average(10,20,30))
---
+++ 4_Python/syntax/module.py
@@ -0,0 +1,7 @@
+'''
+import math2
+
+print(math2.average(1,2,3))
+'''
+
+from math2 import average
